5cc1cf4177c8a660905b9c95beff8e8be92155c5
michaelkimaru <mgkimaru@gmail.com>
2023-02-16 10:09:33 +0300

    entered guesses

--- game.py
+++ game.py
@@ -4,13 +4,21 @@ from random import randint
 
 # get user input
 #generate a random number 
-num=randint(10,50)
+
 # using a while loop, checkif user input = generated number 
 #! get username
 username=input("Enter your name: ")
 print("Hello there "+ username +" ,welcome to guessing game")
-
+num=randint(10,50)
 # generate a random number
 counter=0
-while counter<5:
+while counter <5:
     usernumber=eval(input("Enter guess "))
+    counter += 1
+    if usernumber < num:
+        print("too low")
+    elif usernumber>num:
+        print("too high")
+    elif usernumber==num:
+        print(" you win!")
+        break        
